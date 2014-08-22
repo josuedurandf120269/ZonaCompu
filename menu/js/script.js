ecd26463f05fc40d1bf7f72b8d10134d66bb6422
josueduran-df12069 <deficito.josue@gmail.com>
2014-08-22 16:12:40 -0600

    Mi proyecto
    Mi aporte Josue Duran

---
+++ menu/js/script.js
@@ -0,0 +1,33 @@
+var opcion;
+opcion = confirm('\xBFDesea realizar una busqueda? s/n?');
+
+switch(opcion){
+	case "tablet":
+	function Tablet() {
+	opcion = confirm('\xBFDesea realizar una busqueda de Tablets? s/n?');
+	}
+	break;
+	case "blackberrys":
+	function Blackberry() {
+	opcion = confirm('\xBFDesea realizar una busqueda de Blackberrys? s/n?');
+	}
+	break;
+	case "iphones":
+	function Iphones() {
+	opcion = confirm('\xBFDesea realizar una busqueda de Iphones? s/n?');
+	}
+	break;
+	case "laptops":
+	function Laptops() {
+	opcion = confirm('\xBFDesea realizar una busqueda de Laptops? s/n?');
+	}
+	break;
+	case "ipods":
+	function Ipods() {
+	opcion = confirm('\xBFDesea realizar una busqueda de Ipods ? s/n?');
+	}
+	break;
+	default:
+	alert('');
+	}
+	
